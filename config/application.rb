047bac55cca4538577029d87cb893549d0c189c8
Tim Kinnel <tkinnel@gmail.com>
2014-02-26 00:30:05 -0500

    Remove ckeditor completely from application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,5 @@ module Ofx
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
-    # Add ckeditor path to autload
-#     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-#     config.assets.precompile += Ckeditor.assets
-#     config.assets.precompile += %w(ckeditor/*)
   end
 end
