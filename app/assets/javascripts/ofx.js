e1f51a7588fc5084507a22e76246740600a80e40
Tim Kinnel <tkinnel@gmail.com>
2014-02-27 12:13:10 -0500

    Configuration of tinyMCE

--- app/assets/javascripts/ofx.js
+++ app/assets/javascripts/ofx.js
@@ -7,4 +7,18 @@ $(function() {
   
   //Implement smooth scrolling per page links (requires offset for top bar)
   $('a').smoothScroll({ offset: -60 })
+
+  // Toggle button for tinyMCE
+  $('body').on('click', '.toggle-mce', function(e) {
+    e.preventDefault();
+    var editor_id = $(e.currentTarget).attr('class').split(' ').pop();
+    tinymce.execCommand('mceToggleEditor',false, editor_id);
+  }); 
+
+  tinymce.settings.setup = function(ed) { 
+    ed.on('init', function(ed) {
+      $editor = $(ed.target.contentAreaContainer).parent().parent().parent();
+      $editor.append('<br><p><a href="#" class="btn btn-primary toggle-mce ' + ed.target.id + '"><span>Toggle WYSIWIG/Source</span></a></p>')
+     }); 
+  }
 });
