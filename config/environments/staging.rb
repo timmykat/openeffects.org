6c24394964d53d1eb3c3e493f562f49ae7071666
Tim Kinnel <tkinnel@gmail.com>
2014-11-03 12:15:26 -0500

    Change mailer for staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -79,8 +79,8 @@ Ofx::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Default URL
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.default_options = {from: 'no-reply@openeffects.org'}
-  config.action_mailer.default_url_options = { :host => 'openeffects.org' }
+  config.action_mailer.raise_delivery_errors = true
+  mailconf = Rails.configuration.ofx[:mailer][:development]
+  config.action_mailer.delivery_method = mailconf[:delivery_method]
+  config.action_mailer.smtp_settings = mailconf[:settings]
 end
