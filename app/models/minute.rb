020b735b0f6feb3dd20a5f869ae5ee2d0e5701cf
Tim Kinnel <tkinnel@gmail.com>
2015-05-24 12:33:35 -0400

    Fix meeting type to validate against strings instead of symbols

--- app/models/minute.rb
+++ app/models/minute.rb
@@ -5,7 +5,7 @@ class Minute < ActiveRecord::Base
   include Ofx::HtmlSanitizer
   
   before_validation :sanitize_textareas
-  validates :meeting, :presence => true, :inclusion => Rails.configuration.ofx[:meeting_type].keys
+  validates :meeting, :presence => true, :inclusion => Rails.configuration.ofx[:meeting_type].keys.map {|x| x.to_s}
   validates :date, :presence => true
   validates :members, :presence => true
   validates :minutes, :presence => true
